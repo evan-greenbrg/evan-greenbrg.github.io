dea9a7ac17d571330d30cf1b5a0a806757c7f91d
Evan Greenberg <greenberg@Evans-MacBook-Pro.local>
2022-09-12 16:34:26 -0700

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-ca0deb130c1e20e843f3.js"
+    "url": "webpack-runtime-1084ed993ecf51878f36.js"
   },
   {
     "url": "framework-62abee5b93466abf00bd.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "9a7d58fd0f6d09ca81f5eb26e82ad3e4"
+    "revision": "c6bf4d8b0299f71487c8ec432a2171f2"
   },
   {
     "url": "polyfill-56dfbdecd251ef6566ab.js"
